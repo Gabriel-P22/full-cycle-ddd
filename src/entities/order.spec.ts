bbb007ef7ee80d631904bfe7451eb5a0596c0d7e
Gabriel <paschoalfilho0303@gmail.com>
2025-05-16 14:59:21 -0300

    feature: create unit tests for orders

---
+++ src/entities/order.spec.ts
@@ -0,0 +1,36 @@
+import Order from "./order";
+import OrderItem from "./orderItem";
+
+describe("Order unit tests", () => {
+
+    it("should throw error when id is empty", () => {
+        expect(() => {
+            let order = new Order("", "123", []);
+        }).toThrow("Id is required");
+
+        expect(() => {
+            let order = new Order("123", "", []);
+        }).toThrow("CustomerId is required");
+
+        expect(() => {
+            let order = new Order("123", "123", []);
+        }).toThrow("Items is required");
+    });
+
+
+    it("Should calculate total", () => {
+        const item = new OrderItem("1", "batata", 1);
+        const order = new Order("1", "CUSTOMER:1", [item]);
+
+        let total = order.total();
+
+        expect(total).toBe(1)
+
+        const order2 = new Order("2", "CUSTOMER:1", [item, item]);
+        total = order2.total();
+
+        expect(total).toBe(2)
+    });
+});
+
+
