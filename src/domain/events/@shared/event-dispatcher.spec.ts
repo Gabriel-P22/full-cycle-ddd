ae718bfa346cde6ba539dbdf37ca6f19d35f3f56
Gabriel <paschoalfilho0303@gmail.com>
2025-05-29 18:17:23 -0300

    feature: implemented methods

--- src/domain/events/@shared/event-dispatcher.spec.ts
+++ src/domain/events/@shared/event-dispatcher.spec.ts
@@ -1,4 +1,5 @@
 import { SendEmailWhenProductsIsCreatedHandler } from "../product/handler/send-email-when-product-is-creatred.handler";
+import ProductCreatedEvent from "../product/product-created.event";
 import EventDispatcher from "./event-dispatcher";
 
 describe("Domain events tests", () => {
@@ -11,9 +12,68 @@ describe("Domain events tests", () => {
 
         eventDispatcher.register(eventName, eventHandler);
 
-        expect(eventDispatcher.getEventHandlers[eventName]).toBeDefined()
-        expect(eventDispatcher.getEventHandlers[eventName].length).toBe(1)
+        expect(eventDispatcher.getEventHandlers[eventName]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers[eventName].length).toBe(1);
+        expect(eventDispatcher.getEventHandlers[eventName][0]).toMatchObject(eventHandler);
+    });
 
 
-    })
+    it("should unregister an event handler", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductsIsCreatedHandler();
+
+        const eventName = "ProductCreatedEvent";
+
+        eventDispatcher.register(eventName, eventHandler);
+
+        expect(eventDispatcher.getEventHandlers[eventName]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers[eventName].length).toBe(1);
+        expect(eventDispatcher.getEventHandlers[eventName][0]).toMatchObject(eventHandler);
+
+        eventDispatcher.unregister(eventName, eventHandler);
+        expect(eventDispatcher.getEventHandlers[eventName].length).toBe(0);
+
+    });
+
+    it("should unregister all event handlers", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductsIsCreatedHandler();
+
+        const eventName = "ProductCreatedEvent";
+
+        eventDispatcher.register(eventName, eventHandler);
+
+        expect(eventDispatcher.getEventHandlers[eventName]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers[eventName].length).toBe(1);
+        expect(eventDispatcher.getEventHandlers[eventName][0]).toMatchObject(eventHandler);
+
+        eventDispatcher.unregisterAll();
+        expect(eventDispatcher.getEventHandlers[eventName]).toBeUndefined();
+    });
+
+    it("should notify all event handlers", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductsIsCreatedHandler();
+        const spyEventHandler = jest.spyOn(eventHandler, "handler");
+
+        const eventName = "ProductCreatedEvent";
+
+        eventDispatcher.register(eventName, eventHandler);
+
+        expect(eventDispatcher.getEventHandlers[eventName]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers[eventName].length).toBe(1);
+        expect(eventDispatcher.getEventHandlers[eventName][0]).toMatchObject(eventHandler);
+
+        
+
+        const productCreatedEvent = new ProductCreatedEvent({
+            name: "Product 1",
+            description: "Product 1 description",
+            price: 10.0
+        });
+
+        eventDispatcher.notify(productCreatedEvent);
+
+        expect(spyEventHandler).toHaveBeenCalled();
+    });
 })
--- src/domain/events/@shared/event-dispatcher.ts
+++ src/domain/events/@shared/event-dispatcher.ts
@@ -4,20 +4,41 @@ import EventInterface from "./event.interface";
 
 export default class EventDispatcher implements EventDispatcherInterface {
     
+    private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+    get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+        return this.eventHandlers;
+    }
+
     notify(event: EventInterface): void {
-        throw new Error("Method not implemented.");
+        const eventName = event.constructor.name;
+
+        if (this.eventHandlers[eventName]) {
+            this.eventHandlers[eventName].forEach((eventHandler) => {
+                eventHandler.handler(event);
+            });
+        }
     }
 
     register(eventName: string, event: EventHandlerInterface): void {
-        throw new Error("Method not implemented.");
+        if(!this.eventHandlers[eventName]) {
+            this.eventHandlers[eventName] = []
+        }
+
+        this.eventHandlers[eventName].push(event);
     }
 
     unregister(eventName: string, event: EventHandlerInterface): void {
-        throw new Error("Method not implemented.");
+        if (this.eventHandlers[eventName]) {
+            const index = this.eventHandlers[eventName].indexOf(event);
+            if (index !== -1) {
+                this.eventHandlers[eventName].splice(index, 1);
+            }
+        }
     }
 
     unregisterAll(): void {
-        throw new Error("Method not implemented.");
+        this.eventHandlers = {};
     }
 
 }
